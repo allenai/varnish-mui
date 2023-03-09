fb4349c00c666d2a216915beb8db07ea11126648
jon borchardt <jon.borchardt@gmail.com>
2023-03-09 08:23:09 -0800

    remove movie title by request

--- docs/data/joy/main-features/color-inversion/ColorInversionPopup.js
+++ docs/data/joy/main-features/color-inversion/ColorInversionPopup.js
@@ -112,7 +112,7 @@ const films = [
   { label: 'The Godfather: Part II', year: 1974 },
   { label: 'The Dark Knight', year: 2008 },
   { label: '12 Angry Men', year: 1957 },
-  { label: "Schindler's List", year: 1993 },
+  // { label: "Schindler's List", year: 1993 }, // removed by request from design
   { label: 'Pulp Fiction', year: 1994 },
   {
     label: 'The Lord of the Rings: The Return of the King',
