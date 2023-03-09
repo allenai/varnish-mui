8b8be28db0cb2a988298cc50b6bdb8d65193cc4b
jon borchardt <jon.borchardt@gmail.com>
2023-03-09 15:29:28 -0800

    updated typography and general cleanup

---
+++ packages/varnish/src/theme/paletteExtended.ts
@@ -0,0 +1,11 @@
+import { Indexable } from '../utils/base';
+import { color2 } from './colors';
+
+// add colors here that should be uses as part of a palette that dont exist in the mui palette
+export const paletteExtended = {
+  background: {
+    dark: color2.B5,
+  },
+};
+
+export type PaletteExtendedType = Indexable<typeof paletteExtended>;
