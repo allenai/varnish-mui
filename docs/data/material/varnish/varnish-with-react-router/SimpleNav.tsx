f1976864842b67a1ffb76b410be5b0601d278ac7
Arnavi Chheda <arnavic@Arnavi-Chhedas-MacBook-Pro.local>
2023-03-13 22:54:08 -0700

    docs for using react-router with Varnish

---
+++ docs/data/material/varnish/varnish-with-react-router/SimpleNav.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Header, VarnishApp } from '@allenai/varnish';
+
+import { BrowserRouter, NavLink} from "react-router-dom";
+import { StaticRouter } from 'react-router-dom/server';
+
+// This is something we have to do to play nicely with Material UI's docs pages. 
+// This should not be needed when utilizing React Router on other web pages.
+function Router(props: { children?: React.ReactNode }) {
+  const { children } = props;
+  if (typeof window === 'undefined') {
+    return <StaticRouter location="/">{children}</StaticRouter>;
+  }
+
+  return <BrowserRouter>{children}</BrowserRouter>;
+}
+
+export default function Demo() {
+  return (
+    <VarnishApp>
+      <div style={{ width: '100%' }}>
+        <Header.AI2Banner />
+        <Router>
+          <ul>
+            <li>
+              <NavLink reloadDocument className={"linkContrast"} to="/material-ui/getting-started/overview/">To Getting Started</NavLink>
+            </li>
+            <li>
+              <NavLink reloadDocument to="/material-ui/varnish/app-layout">To Layout</NavLink>
+            </li>
+          </ul>
+        </Router>
+      </div>
+    </VarnishApp>
+  );
+}
