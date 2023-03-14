d04ae3488e8a88cdc37fcac3d5dfb3c64dc6cdf0
Arnavi Chheda <arnavic@Arnavi-Chhedas-MacBook-Pro.local>
2023-03-14 12:44:45 -0700

    adding docs for extended palette

---
+++ docs/data/material/varnish/colors/PaletteExtendedDemo.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { styled, css } from '@mui/material/styles';
+import Link from '@mui/material/Link';
+
+export default function Demo() {
+    const BackgroundDiv = styled('div')(
+        ({ theme }) =>
+            css`
+        width: 100%;
+        text-align: center;
+        margin-bottom: ${theme.spacing(0.5)};
+        background: ${theme.paletteExtended.background.dark.hex};
+      `,
+    );
+
+    return (
+        <div style={{ width: '100%' }}>
+            <BackgroundDiv>
+                <Link variant="dark" href="https://allenai.org">
+                    © The Allen Institute for Artificial Intelligence
+                </Link>
+            </BackgroundDiv>
+        </div>
+    );
+}
