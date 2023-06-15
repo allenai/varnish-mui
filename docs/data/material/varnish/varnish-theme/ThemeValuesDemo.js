d018a9d4b184c66c6a4f7aac13f054bbce2d4b78
Arnavi Chheda <arnavic@live.com>
2023-06-15 16:40:42 -0400

    Update Theme docs (#124)
    * removing unstable theme objects from docs, and adding instructions for spacing
    
    * comment
    
    ---------
    
    Co-authored-by: Arnavi Chheda <arnavic@arnavichedasmbp.myfiosgateway.com>

--- docs/data/material/varnish/varnish-theme/ThemeValuesDemo.js
+++ docs/data/material/varnish/varnish-theme/ThemeValuesDemo.js
@@ -5,7 +5,7 @@
 import * as React from 'react';
 import { getTheme } from '@allenai/varnish2';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails, Link } from '@mui/material';
 
 export default function Demo() {
   const theme = getTheme();
@@ -13,16 +13,31 @@ export default function Demo() {
 
   return (
     <div style={{ maxWidth: '100%' }}>
-      {topLevelKeys.map((key) => (
-        <Accordion key={key}>
-          <AccordionSummary expandIcon={<ExpandMore />}>{key}</AccordionSummary>
-          <AccordionDetails>
-            <pre style={{ whiteSpace: 'pre-wrap' }}>
-              {JSON.stringify(theme[key], null, 2)}
-            </pre>
-          </AccordionDetails>
-        </Accordion>
-      ))}
+      {topLevelKeys.map(
+        (key) =>
+          !key.startsWith('unstable') && (
+            <Accordion key={key}>
+              <AccordionSummary expandIcon={<ExpandMore />}>{key}</AccordionSummary>
+              <AccordionDetails>
+                {key === 'spacing' ? (
+                  <pre style={{ whiteSpace: 'pre-wrap' }}>
+                    <p>
+                      We are using the Material spacing function and guidelines. See
+                      Material docs for how to use spacing:
+                    </p>
+                    <Link href="https://varnish.allenai.org/material-ui/customization/spacing/">
+                      https://varnish.allenai.org/material-ui/customization/spacing/
+                    </Link>
+                  </pre>
+                ) : (
+                  <pre style={{ whiteSpace: 'pre-wrap' }}>
+                    {JSON.stringify(theme[key], null, 2)}
+                  </pre>
+                )}
+              </AccordionDetails>
+            </Accordion>
+          ),
+      )}
     </div>
   );
 }
